381c231cfb4baa2f6a0678fb5b520c89608d9c55
Puck5150 <kbarnett25@gmail.com>
2026-08-05 10:13:12 -0500

    feat: register tauri-plugin-updater and tauri-plugin-process

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -38,6 +38,8 @@ pub fn run() {
     .plugin(tauri_plugin_fs::init())
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_notification::init())
+    .plugin(tauri_plugin_updater::Builder::new().build())
+    .plugin(tauri_plugin_process::init())
     .setup(|app| {
       use tauri::Manager;
       let root = app.path().app_data_dir()?;
